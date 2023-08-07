191644303bca9fb964a631f7dca351e924c3a79a
Yash067 <harneyash067@gmail.com>
2023-08-07 10:25:50 +0530

    added more functions to icon and fixed click anywhere problem partially

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let starticon = document.getElementsByClassName("starticon")[0]
 let startmenu = document.getElementsByClassName("startmenu")[0]
+let searchicon = document.getElementsByClassName("searchicon")[0]
+let searchmenu = document.getElementsByClassName("searchmenu")[0]
 let widget = document.getElementsByClassName("widget")[0]
-let widgeticn = document.getElementsByClassName("left")[0]
+let widgeticon = document.getElementsByClassName("left")[0]
+let body = document.getElementsByClassName("body")[0]
+
 
 starticon.addEventListener("click", ()=>{
     if(startmenu.style.bottom == "50px"){
@@ -12,7 +16,29 @@ starticon.addEventListener("click", ()=>{
     }
 })
 
-widgeticn.addEventListener("click", ()=>{
+document.addEventListener("click", (event) => {
+    if (!startmenu.contains(event.target) && event.target !== starticon ) {
+        startmenu.style.bottom = "-665px";
+    }
+});
+
+searchicon.addEventListener("click", ()=>{
+    if(searchmenu.style.bottom =="50px"){
+        searchmenu.style.bottom ="-665px"
+    }
+    else{
+        searchmenu.style.bottom = "50px"
+    }
+
+})
+
+document.addEventListener("click", (event) => {
+    if (!searchmenu.contains(event.target) && event.target !== searchicon ) {
+        searchmenu.style.bottom = "-665px";
+    }
+});
+
+widgeticon.addEventListener("click", ()=>{
     if(widget.style.left == "10px"){
         widget.style.left = "-915px"
     }
@@ -20,3 +46,9 @@ widgeticn.addEventListener("click", ()=>{
         widget.style.left = "10px"
     }
 })
+
+document.addEventListener("click", (event) => {
+    if (!widget.contains(event.target) && event.target !== widgeticon ) {
+        widget.style.left = "-915px";
+    }
+});
